60c59345aa60c412634e2890a611c26abd644795
Anirban Kundu <kunduani2003@gmail.com>
2025-09-17 12:44:56 +0530

    CHAGE IN NEXT CONFIG

--- next.config.ts
+++ next.config.ts
@@ -8,7 +8,7 @@ const nextConfig: NextConfig = {
     return [
       {
         source: '/api/:path*',
-        destination: 'http://127.0.0.1:8000/api/:path*',
+        destination: `${process.env.NEXT_PUBLIC_API_URL}/:path*`,
       },
     ];
   },
