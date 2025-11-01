76e5aa30626eb333e7303dddca0d981b9dca78a7
Anirban Kundu <67548198+techanirbankundu@users.noreply.github.com>
2025-11-01 08:18:47 +0530

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   env: {
-    NEXT_PUBLIC_API_URL: process.env.NEXT_PUBLIC_API_URL || 'http://127.0.0.1:8000/api',
+    NEXT_PUBLIC_API_URL: process.env.NEXT_PUBLIC_API_URL || 'https://ragillybaceknd.vercel.app/api',
   },
   async rewrites() {
     return [
